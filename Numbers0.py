79b6aa25a5334444988c49db5755a222ec568f8b
tonydebling <tonydebling@gmail.com>
2016-03-01 12:39:45 +0000

    Update Numbers0.py

--- Numbers0.py
+++ Numbers0.py
@@ -1,4 +1,8 @@
 # Numbers0.py
+# Run this program
+# Enter the same pair of numbers for both sets of inputs
+# Are these the results that you expect?
+# Can you explain why they are different?
 
 print("**** First Example ****")
 number1 = input("Enter your first number: ")
@@ -10,9 +14,10 @@ number1 = int(input("Enter your first number: "))
 number2 = int(input("Enter your second number: "))
 print("Answer = ", number1 + number2)
 
+"""
 print("**** Third Example ****")
 number1 = float(input("Enter your first number: "))
 number2 = float(input("Enter your second number: "))
 print("Answer = ", number1 + number2)
-
+"""
 
